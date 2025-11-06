9318a42b2ab3a5e6b206fb149b4a962aa111188f
Grace Wambui <wambuigm51@yahoo.com>
2025-11-06 12:12:58 +0300

    feat(auth): implement sign-in and sign-up forms with form components

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import InputField from '@/components/forms/InputField';
+import FooterLink from '@/components/forms/FooterLink';
+// import {signInWithEmail} from "@/lib/actions/auth.actions";
+// import {toast} from "sonner";
+import {useRouter} from "next/navigation";
+
+const SignIn = () => {
+    const router = useRouter
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitting },
+        } = useForm<SignInFormData>({
+        defaultValues: {
+            email: '',
+            password: '',
+        }, 
+        mode: 'onBlur'
+        })
+    const onSubmit = async (data: SignInFormData) => {
+    try {
+        // const result = await signInWithEmail(data)
+        // if (result.success) {
+        //     router.push('/')
+        // }
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+        // toast.error('Sign in failed', {
+        //     description: error instanceof Error ? error.message: 'Failed to sign in'
+        // })
+    }
+    }
+  return (
+    <>
+        <h1 className='form-title'>Welcome back!</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+            <InputField 
+                name='email'
+                label='Email'
+                placeholder='janedoe@email.com'
+                register={register}
+                error={errors.email}
+                validation={{required: 'Email is required', pattern: /^w+@\w+\.\w+$/, message: 'Email address is required'}}
+            /> 
+            <InputField 
+                name='password'
+                label='Password'
+                placeholder='Enter a password'
+                type='password'
+                register={register}
+                error={errors.password}
+                validation={{required: 'Password is required', minLength: 8}}
+            />
+
+            <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+                {isSubmitting ? 'Signing In' : 'Sign In'}
+            </Button>
+
+            <FooterLink text="Don't have an account?" linkText='Sign up' href='/sign-up'/>
+        </form>
+    </>
+  )
+}
+
+export default SignIn
+
